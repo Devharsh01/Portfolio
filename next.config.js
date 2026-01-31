9a670eeaaafc48b02023a02f5c1c69923686a540
Dev Harsh Agarwal <devharsh213@gmail.com>
2026-01-31 22:17:31 +0530

    Minute Changes

--- next.config.js
+++ next.config.js
@@ -6,16 +6,7 @@ const nextConfig = {
   optimizeFonts: false,
   images: {
     domains: ["user-images.githubusercontent.com", "cdn.hashnode.com", "github.com"],
-  },
-  fontLoaders: [
-    {
-      loader: "@next/font/google",
-      options: {
-        subsets: ["latin"],
-        weight: ["400", "500", "600", "700", "800"],
-      },
-    },
-  ],
+  }
 };
 
 module.exports = nextConfig;
